3282b565b7cb3e71f74831f89b66a17b99ef6477
Jamie Ly <me@jamie.ly>
2013-06-20 19:12:16 -0400

    Removed unnecessary stuff from G2Basic

--- src/organisms/g2/behaviors/ExploreBehavior.java
+++ src/organisms/g2/behaviors/ExploreBehavior.java
@@ -2,6 +2,7 @@ package organisms.g2.behaviors;
 
 import organisms.Move;
 import organisms.g2.PlayerBase;
+import organisms.g2.PlayerUtil;
 import organisms.g2.data.MoveInput;
 
 public class ExploreBehavior extends BehaviorBase {
@@ -13,7 +14,18 @@ public class ExploreBehavior extends BehaviorBase {
 		if(iDontKnowWhatToDo()) return null;
 		if(itIsBetterToStayPut()) return getMoveFactory().stayPutMove();
 		
+		return new Move(getNextMoveDirection());
+	}
 	
+	protected int getNextMoveDirection() {
+		int lastDirection = WEST;
+		if(getMemory().hasLastDirection() && getMemory().getLastDirection() != STAYPUT) {
+			lastDirection = getMemory().getLastDirection();
+		}
+		if(getPlayer().nOutOfMTimes(2, 10)) {
+			lastDirection = PlayerUtil.oppositeDirection(lastDirection);
+		}
+		return lastDirection;
 	}
 	
 	protected Boolean iDontKnowWhatToDo() {
