a3b55195cb4b762091edcbdb160d256a9333c5ae
Jamie Ly <me@jamie.ly>
2013-06-22 13:30:56 -0400

    Refactored waypoint factory for future patterns

--- src/organisms/g2/behaviors/CoordinatorFactory.java
+++ src/organisms/g2/behaviors/CoordinatorFactory.java
@@ -16,4 +16,12 @@ public class CoordinatorFactory {
 		coordinator.addBehavior(WaypointBehavior.squareWaypointBehavior(player));
 		return coordinator;
 	}
+	public static BehaviorCoordinator hungryReproduceWaypointCoordinator(PlayerBase player) {
+		BehaviorCoordinator coordinator = new BehaviorCoordinator();
+		coordinator.addBehavior(new HungryBehavior(player));
+		coordinator.addBehavior(new ReproductionBehavior(player));
+		coordinator.addBehavior(WaypointBehavior.squareWaypointBehavior(player));
+		coordinator.addBehavior(new RandomBehavior(player));
+		return coordinator;
+	}
 }
--- src/organisms/g2/waypoints/WaypointListFactory.java
+++ src/organisms/g2/waypoints/WaypointListFactory.java
@@ -1,18 +1,72 @@
 package organisms.g2.waypoints;
 
+import java.util.ArrayList;
+
+import organisms.Constants;
 import organisms.g2.data.Point;
 
-public class WaypointListFactory {
-	public static WaypointList squareWaypointList() {
-		Point[] pts = new Point[] {
-				new Point(4, 0),
-				new Point(0, 4),
-				new Point(-4, 0),
-				new Point(0, -4)
-		};
-		return listFromPoints(pts);
+public class WaypointListFactory implements Constants {
+	
+	protected static Point pointNorth(int magnitude) {
+		return new Point(0, magnitude);
 	}
-	public static WaypointList listFromPoints(Point[] points) {
+	
+	protected static Point pointSouth(int magnitude) {
+		return new Point(0, -magnitude);
+	}
+	
+	protected static Point pointEast(int magnitude) {
+		return new Point(magnitude, 0);
+	}
+	
+	protected static Point pointWest(int magnitude) {
+		return new Point(-magnitude, 0);
+	}
+	
+	protected static ArrayList<Point> pointSquareStartingWithDirectionWithMagnitude(int direction, Boolean clockwise, int magnitude) {
+		Integer[] directions = clockDirectionsStartingWith(direction, clockwise);
+		ArrayList<Point> points = new ArrayList<Point>();
+		for(Integer dir: directions) {
+			points.add(pointInDirection(dir, magnitude));
+		}
+		return points;
+	}
+	
+	protected static Integer[] clockDirectionsStartingWith(Integer direction, Boolean clockwise) {
+		final int[] counterClockwiseDirections = new int[]{NORTH, WEST, SOUTH, EAST};
+		final int[] clockwiseDirections = new int[]{NORTH, EAST, SOUTH, WEST};
+		
+		int[] directions = clockwise ? clockwiseDirections : counterClockwiseDirections;
+		// find the index of the passed direction
+		int indexOfPassedDirection = -1;
+		for(int i = 0, size = 4; i < size; i++) {
+			if(directions[i] != direction) continue;
+			indexOfPassedDirection = i;
+			break;
+		}
+		
+		int size = 4;
+		Integer[] revisedDirections = new Integer[size];
+		for(int i = 0; i < size; i++) {
+			revisedDirections[i] = directions[(indexOfPassedDirection + i) % size];
+		}
+		return revisedDirections;
+	}
+	
+	protected static Point pointInDirection(int direction, int magnitude) {
+		switch(direction) {
+		case NORTH: return pointNorth(magnitude);
+		case SOUTH: return pointSouth(magnitude);
+		case EAST: return pointEast(magnitude);
+		case WEST: return pointWest(magnitude);
+		}
+		return Point.origin();
+	}
+	
+	public static WaypointList squareWaypointList() {
+		return listFromPoints(pointSquareStartingWithDirectionWithMagnitude(EAST, false, 4));
+	}
+	public static WaypointList listFromPoints(Iterable<Point> points) {
 		WaypointList list = new WaypointList();
 		for(Point p: points) {
 			list.addWaypoint(new Waypoint(p));
